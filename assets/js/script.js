859d8789d876ed95b1ba2395104146e4fbbbeb39
pez57 <perry_s@live.com>
2022-01-07 15:43:20 +0000

    Start javaScript listeners

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// const DOM elements
+
+const buttons = document.getElementsByClassName("move");
+const moves = ["rock", "paper", "scissors", "lizard", "spock"];
+const yourScore = document.getElementById("your-score");
+const botScore = document.getElementById("bot-score");
+
+
+// Add event listeners to buttons
+
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+
+    })
+}
+
+/*
+function playGame()
+
+
+function playerMove()
+
+
+function robotMove()
+
+
+function calculateWinner()
+
+
+function iterateScore()
+
+
+function roundWinner()
+*/
