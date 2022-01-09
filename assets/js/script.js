f56f7aa4a43f52e72a182ce547cf90b5f4661ca4
pez57 <perry_s@live.com>
2022-01-09 20:15:05 +0000

    Add event lister function and player choice function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
 // const DOM elements
 
-const buttons = document.getElementsByClassName("move");
+const moveButtons = document.getElementsByClassName("move");
 const moves = ["rock", "paper", "scissors", "lizard", "spock"];
 const yourScore = document.getElementById("your-score");
 const botScore = document.getElementById("bot-score");
+const resetButton = document.getElementById("reset-button");
+const playerChoice = document.getElementById("player-choice");
+const robotChoice = document.getElementById("robot-choice");
 
 
 // Add event listeners to buttons
-
-for (let button of buttons) {
-    button.addEventListener("click", function() {
-
-    })
+//target vales to be removed on html
+for (let button of moveButtons) {
+    button.addEventListener("click", function(event) {
+     event.preventDefault();
+     let move = this.getAttribute("data-type");
+     playGame(move);
+    });
 }
 
-/*
-function playGame()
+function playGame(playerMove) {
+    playerChoice.innerHTML = `<div>You chose ${playerMove}</div>
+    <i class="far fa-hand-${playerMove}"></i>
+    `;
+    let robotRandom = Math.floor(Math.random() * 5);
 
+}
 
-function playerMove()
+/* function playerMove()
 
 
 function robotMove()
